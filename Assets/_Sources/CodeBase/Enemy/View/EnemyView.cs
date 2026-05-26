ab26342cb833d5fc036196db7bc84984d34e683d
BratAsakura <brat09141@gmail.com>
2026-05-26 13:43:34 +0300

    [feat] - добавленны отображение уровня и хп

---
+++ Assets/_Sources/CodeBase/Enemy/View/EnemyView.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class EnemyView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _levelUI;
+    [SerializeField] private TextMeshProUGUI _healthUI;
+
+    private Enemy enemy;
+
+    private void Awake()
+    {
+        enemy = GetComponent<Enemy>();
+        _healthUI.SetText($"Health: {enemy.Health}");
+        _levelUI.SetText($"Level: {enemy.Level}");
+    }
+
+    private void OnEnable()
+    {
+        enemy.HealthChanged += OnHealthView;
+        enemy.LevelUpdate += OnLevelView;
+    }
+
+    private void OnDestroy()
+    {
+        enemy.HealthChanged += OnHealthView;
+        enemy.LevelUpdate += OnLevelView;
+    }
+
+    private void OnLevelView(int value)
+    {
+        _levelUI.SetText($"Level: {value}");
+    }
+
+    private void OnHealthView(float value)
+    {
+        _healthUI.SetText($"Health: {value}");
+    }
+}

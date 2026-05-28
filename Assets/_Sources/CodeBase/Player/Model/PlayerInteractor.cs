6d4f52434a2e5ee74be4b4a1ee1804942255855c
BratAsakura <brat09141@gmail.com>
2026-05-28 17:05:46 +0300

    [refactor] Сделал пока заглушку

--- Assets/_Sources/CodeBase/Player/Model/PlayerInteractor.cs
+++ Assets/_Sources/CodeBase/Player/Model/PlayerInteractor.cs
@@ -1,10 +1,10 @@
+using System;
 using UnityEngine;
 
 public class PlayerInteractor : MonoBehaviour
 {
-
     public void Interact(IInteractable target)
     {
-
+        throw new NotImplementedException();
     }
 }
